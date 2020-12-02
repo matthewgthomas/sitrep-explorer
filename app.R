56aa71b2b38e41fcf9a511d6a01c1046f12af87d
Matt Thomas <matthewgthomas@gmail.com>
2020-12-02 19:45:59 +0000

    Show England trends

---
+++ app.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(arrow)
+
+eng_2021 = read_feather("data/england-2020-21.feather")
+eng_hist_sum = read_feather("data/england-historical.feather")
+trust_2021 = read_feather("data/trusts-2020-21.feather")
+trust_hist_sum = read_feather("data/trusts-historical.feather")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    titlePanel("NHS England winter situation report explorer"),
+
+    sidebarLayout(
+        sidebarPanel(
+            radioButtons("eng_or_trusts", "Show data for England or individual Trusts?", choices = c("England", "Trusts")),
+
+            selectInput("trust_name", "Select a Trust", sort(trust_2021$Name), multiple = FALSE),
+
+            radioButtons("trust_comparison", "Compare this Trust to...", choices = c("Itself historically", "Other Trusts this year", "England averages"))
+        ),
+
+        mainPanel(
+           plotOutput("plot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$plot <- renderPlot({
+        if (input$eng_or_trusts == "England") {
+            # Plot bed occupancy trends for England
+            eng_hist_sum %>%
+                ggplot(aes(x = day_of_year, y = `Median occupancy rate`, group = 1)) +
+                geom_ribbon(aes(ymin = `Min occupancy rate`, ymax = `Max occupancy rate`), fill = "grey", alpha = 0.4) +
+                geom_line(colour = "grey", lty = 2, size = 1.1) +
+
+                geom_line(data = eng_2021, aes(y = `Occupancy rate`), colour = "red", size = 1.1) +
+
+                scale_y_continuous(labels = scales::percent) +
+                scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 week", date_labels = "%B") +
+
+                labs(title = "Bed occupancy rates in England",
+                     subtitle = "Red line shows rates in 2020-21; grey lines show historical average, minimum and maximum rates",
+                     x = NULL, y = "Bed occupancy rate",
+                     caption = "Source: BRC/I&I analysis of NHSE data") +
+                theme_classic()
+
+        } else {
+            # User wants to look at Trusts
+
+
+
+        }
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
