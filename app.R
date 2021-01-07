0eb41f13577869e46efbaf47b20da9721886ef40
Matt Thomas <matthewgthomas@gmail.com>
2021-01-07 11:55:51 +0000

    Bug fix in displaying historical Trust data

--- app.R
+++ app.R
@@ -14,9 +14,9 @@ ui <- fluidPage(
 
     sidebarLayout(
         sidebarPanel(
-            radioButtons("eng_or_trusts", "Show data for England or individual Trusts?", choices = c("England", "Trusts")),
+            radioButtons("eng_or_trusts", "Show data for England or for individual Trusts?", choices = c("England", "Trusts")),
 
-            selectInput("trust_name", "Select a Trust", sort(trust_2021$Name), multiple = FALSE),
+            selectInput("trust_name", "Select a Trust", sort(trust_2021$Name), selected = sort(trust_2021$Name)[1], multiple = FALSE),
 
             radioButtons("trust_comparison", "Compare this Trust to...", choices = c("Itself historically", "Other Trusts this year", "England averages"))
         ),
@@ -53,23 +53,38 @@ server <- function(input, output) {
             # User wants to look at Trusts
             # What do they want to compare the selected Trust to?
             if (input$trust_comparison == "Itself historically") {
-                trust_hist_sum %>%
-                    filter(Name == input$trust_name) %>%
 
-                    ggplot(aes(x = day_of_year, y = `Median occupancy rate`, group = 1)) +
-                    geom_ribbon(aes(ymin = `Min occupancy rate`, ymax = `Max occupancy rate`), fill = "grey", alpha = 0.4) +
-                    geom_line(colour = "grey", lty = 2, size = 1.1) +
-
-                    geom_line(data = trust_2021 %>% filter(Name == input$trust_name),
-                              aes(y = `Occupancy rate`), colour = "red", size = 1.1) +
+                plt <-
+                    ggplot() +
 
                     scale_y_continuous(labels = scales::percent) +
                     scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 week", date_labels = "%B") +
 
-                    labs(title = paste0("Current and historical bed occupancy rates in ", input$trust_name),
+                    labs(title = paste0("Current and historical bed occupancy rates in "), #$trust_name),
                          subtitle = "Red line shows rates in 2020-21; grey lines show historical average, minimum and maximum rates",
                          x = NULL, y = "Bed occupancy rate", caption = "Source: BRC/I&I analysis of NHSE data") +
                     theme_classic()
+
+                this_trust_hist <- trust_hist_sum %>%
+                    filter(Name == input$trust_name)
+                    # filter(Name == trust_2021$Name[1])
+
+                if (nrow(this_trust_hist) > 0) {
+                    plt <-
+                        plt +
+                        geom_ribbon(data = this_trust_hist,
+                                    aes(x = day_of_year, y = `Median occupancy rate`,
+                                        ymin = `Min occupancy rate`, ymax = `Max occupancy rate`), fill = "grey", alpha = 0.4) +
+                        geom_line(data = this_trust_hist,
+                                  aes(x = day_of_year, y = `Median occupancy rate`, group = 1),
+                                  colour = "grey", lty = 2, size = 1.1)
+                }
+
+                plt +
+                    geom_line(data = trust_2021 %>%
+                                  # filter(Name == trust_2021$Name[2]),
+                                  filter(Name == input$trust_name),
+                              aes(x = day_of_year, y = `Occupancy rate`), colour = "red", size = 1.1)
 
             } else if (input$trust_comparison == "Other Trusts this year") {
                 trust_2021 %>%
