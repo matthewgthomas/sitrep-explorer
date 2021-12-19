e1224f37d3c77e3eb2d0e207d7a3eee6dabef920
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2021-12-19 10:28:46 +0000

    Removed old code

--- app.R
+++ app.R
@@ -4,16 +4,9 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
-# library(patchwork)
 library(shinycssloaders)
 
-# eng_2122 <- read_feather("data/england-2021-22.feather")
-# eng_2021 <- read_feather("data/england-2020-21.feather")
-# eng_hist_sum <- read_feather("data/england-historical.feather")
-# trust_2122 <- read_feather("data/trusts-2021-22.feather")
-# trust_2021 <- read_feather("data/trusts-2020-21.feather")
-# trust_hist_sum <- read_feather("data/trusts-historical.feather")
-
+# ---- Load data ----
 england <- read_feather("data/england.feather")
 england_beds <- read_feather("data/england-beds.feather")
 trusts <- read_feather("data/trusts.feather")
@@ -59,38 +52,6 @@ trusts <-
       TRUE ~ "grey"
     )
   )
-
-# ---- Pre-wrangle bed occupancy (count) data ----
-# beds_eng_2122 <- eng_2122 %>%
-#   mutate(`Beds free` = `G&A Beds Open` - `G&A beds occ'd`) %>%
-#   select(day_of_year, `Beds occupied` = `G&A beds occ'd`, `Beds free`) %>%
-#   pivot_longer(cols = -day_of_year)
-#
-# beds_eng_2021 <- eng_2021 %>%
-#   mutate(`Beds free` = `G&A Beds Open` - `G&A beds occ'd`) %>%
-#   select(day_of_year, `Beds occupied` = `G&A beds occ'd`, `Beds free`) %>%
-#   pivot_longer(cols = -day_of_year)
-#
-# beds_eng_hist <- eng_hist_sum %>%
-#   mutate(`Beds free` = `Median beds open` - `Median beds occupied`) %>%
-#   select(day_of_year, `Beds occupied` = `Median beds occupied`, `Beds free`) %>%
-#   pivot_longer(cols = -day_of_year)
-#
-# beds_trust_2122 <- trust_2122 %>%
-#   mutate(`Beds free` = `G&A Beds Open` - `G&A beds occ'd`) %>%
-#   select(day_of_year, Name, `Beds occupied` = `G&A beds occ'd`, `Beds free`) %>%
-#   pivot_longer(cols = -c(day_of_year, Name))
-#
-# beds_trust_2021 <- trust_2021 %>%
-#   mutate(`Beds free` = `G&A Beds Open` - `G&A beds occ'd`) %>%
-#   select(day_of_year, Name, `Beds occupied` = `G&A beds occ'd`, `Beds free`) %>%
-#   pivot_longer(cols = -c(day_of_year, Name))
-#
-# beds_trust_hist <- trust_hist_sum %>%
-#   mutate(`Beds free` = `Median beds open` - `Median beds occupied`) %>%
-#   select(day_of_year, `Beds occupied` = `Median beds occupied`, `Beds free`) %>%
-#   pivot_longer(cols = -day_of_year)
-
 
 # ---- Plot functions ----
 plot_trends <- function(d, indicator, indicator_name, trust_name = NULL, plotting_rates = TRUE) {
@@ -151,25 +112,8 @@ plot_trends <- function(d, indicator, indicator_name, trust_name = NULL, plottin
     theme_classic()
 }
 
+# Plot bar graphs for bed occupancy counts
 plot_counts <- function(d, indicator_name, trust_name = NULL) {
-  # Set up dataframes to use for plotting
-  # d_2122 <- NULL
-  # d_2021 <- NULL
-  # d_hist <- NULL
-  #
-  # if (is.null(trust_name)) {
-  #   d_2122 <- beds_england_2122
-  #   d_2021 <- beds_england_2021
-  #   d_hist <- beds_england_historical
-  #
-  # } else {
-  #   d_2122 <- beds_trusts_2122 %>% filter(Name == trust_name)
-  #   d_2021 <- beds_trusts_2021 %>% filter(Name == trust_name)
-  #   d_hist <- beds_trusts_historical %>% filter(Name == trust_name)
-  #
-  # }
-
-  # max_beds <- max(max(d_2122$value), max(d_2021$value), max(d_hist$value))
   max_beds <- max(d$value, na.rm = TRUE)
 
   place <- ifelse(is.null(trust_name), "England", trust_name)
@@ -185,7 +129,6 @@ plot_counts <- function(d, indicator_name, trust_name = NULL) {
 
     labs(
       title = paste0(indicator_name, " in ", place),
-      # subtitle = "2021-22",
       x = NULL,
       y = "Number of beds",
       fill = NULL,
@@ -193,74 +136,9 @@ plot_counts <- function(d, indicator_name, trust_name = NULL) {
     ) +
     theme_classic() +
     theme(legend.position = "bottom")
-
-  # Plot bar graphs for bed occupancy counts
-  # plt_beds_2122 <-
-  #   d_2122 %>%
-  #   ggplot(aes(x = day_of_year, y = value, fill = name)) +
-  #   geom_col(show.legend = FALSE) +
-  #
-  #   scale_y_continuous(labels = scales::comma, limits = c(0, max_beds)) +
-  #
-  #   labs(
-  #     title = " ",
-  #     subtitle = "2021-22",
-  #     x = NULL,
-  #     y = "Number of beds",
-  #     fill = NULL
-  #     # caption = "Source: BRC/I&I analysis of NHSE data"
-  #   ) +
-  #   theme_classic() +
-  #   theme(legend.position = "bottom")
-  #
-  # plt_beds_2021 <-
-  #   d_2021 %>%
-  #   ggplot(aes(x = day_of_year, y = value, fill = name)) +
-  #   geom_col() +
-  #
-  #   scale_y_continuous(labels = scales::comma, limits = c(0, max_beds)) +
-  #
-  #   labs(
-  #     title = " ",
-  #     subtitle = "2020-21",
-  #     x = NULL,
-  #     y = "Number of beds",
-  #     fill = NULL
-  #     # caption = "Source: BRC/I&I analysis of NHSE data"
-  #   ) +
-  #   theme_classic() +
-  #   theme(legend.position = "bottom")
-  #
-  # plt_beds_hist <-
-  #   d_hist %>%
-  #   ggplot(aes(x = day_of_year, y = value, fill = name)) +
-  #   geom_col(show.legend = FALSE) +
-  #
-  #   scale_y_continuous(labels = scales::comma, limits = c(0, max_beds)) +
-  #
-  #   labs(
-  #     title = " ",
-  #     subtitle = "Averages from 2012-13 to 2019-20",
-  #     x = NULL,
-  #     y = "Number of beds",
-  #     fill = NULL,
-  #     caption = "Source: BRC/I&I analysis of NHSE data"
-  #   ) +
-  #   theme_classic() +
-  #   theme(legend.position = "bottom")
-  #
-  # plt_beds_2122 +plt_beds_2021 + plt_beds_hist
 }
 
 plot_trust_comparison_trends <- function(d, indicator, indicator_name, trust_name, this_year = "2021-22", plotting_rates = TRUE) {
-  # this_trust <-
-  #   trusts %>%
-  #   filter(Name == trust_name & year == year)
-  #
-  # other_trusts <-
-  #   trusts %>%
-  #   filter(Name != trust_name & year == year)
-
   # Set y axis limit to 100% if plotting rates
   y_axis_format <- NULL
   y_limits <- NULL
