460fed6c19082f814e737a5a336de4cb33157539
Matt Thomas <matthewgthomas@gmail.com>
2022-12-02 13:37:14 +0000

    Update data

--- prep data.R
+++ prep data.R
@@ -8,22 +8,22 @@ library(sf)
 library(NHSWinterSitreps)
 
 # ---- Load sitreps ----
-sitrep2122 <- load_sitreps("2021-22")
-sitrep2021 <- load_sitreps("2020-21")
-sitrep1920 <- load_sitreps("2019-20")
-sitrep1819 <- load_sitreps("2018-19")
-sitrep1718 <- load_sitreps("2017-18")
-sitrep1617 <- load_sitreps("2016-17")
-sitrep1516 <- load_sitreps("2015-16")
+# sitrep2122 <- load_sitreps("2021-22")
+# sitrep2021 <- load_sitreps("2020-21")
+# sitrep1920 <- load_sitreps("2019-20")
+# sitrep1819 <- load_sitreps("2018-19")
+# sitrep1718 <- load_sitreps("2017-18")
+# sitrep1617 <- load_sitreps("2016-17")
+# sitrep1516 <- load_sitreps("2015-16")
 
 # ---- Variable conversions ----
-sitrep2021 <-
-  sitrep2021 %>%
-  mutate(across(-c(Code:Date), as.numeric))
-
-sitrep2122 <-
-  sitrep2122 %>%
-  mutate(across(-c(Code:Date), as.numeric))
+# sitrep2021 <-
+#   sitrep2021 %>%
+#   mutate(across(-c(Code:Date), as.numeric))
+#
+# sitrep2122 <-
+#   sitrep2122 %>%
+#   mutate(across(-c(Code:Date), as.numeric))
 
 # ---- Get England-level data ----
 # - Helper functions -
@@ -31,11 +31,11 @@ get_england <- function(d, two_englands = FALSE) {
   if (two_englands) {
     d <-
       d %>%
-      filter(str_detect(Name, "ENGLAND \\(All Acute Trusts\\)"))
+      filter(str_detect(toupper(Name), "ENGLAND \\(ALL ACUTE TRUSTS\\)"))
   } else {
     d <-
       d %>%
-      filter(str_detect(Name, "ENGLAND"))
+      filter(str_detect(toupper(Name), "ENGLAND"))
   }
 
   d %>%
@@ -45,6 +45,7 @@ get_england <- function(d, two_englands = FALSE) {
 
 # - Get all England data -
 england <- bind_rows(
+  get_england(sitrep2223),
   get_england(sitrep2122, TRUE),
   get_england(sitrep2021, TRUE),
   get_england(sitrep1920),
@@ -55,11 +56,12 @@ england <- bind_rows(
 )
 
 # - Summarise by day/month -
-# Set all dates to be in one year so this historical data overlays the 2021-22 data on the graphs
+# Set all dates to be in one year so this historical data overlays the 2022-23 data on the graphs
 england <-
   england %>%
   mutate(
     year = case_when(
+      Date >= dmy("01-11-2022") & Date <= dmy("01-05-2023") ~ "2022-23",
       Date >= dmy("01-11-2021") & Date <= dmy("01-05-2022") ~ "2021-22",
       Date >= dmy("01-11-2020") & Date <= dmy("01-05-2021") ~ "2020-21",
       Date >= dmy("01-11-2019") & Date <= dmy("01-05-2020") ~ "2019-20",
@@ -69,8 +71,8 @@ england <-
       Date >= dmy("01-11-2015") & Date <= dmy("01-05-2016") ~ "2015-16"
     ),
     day_of_year = if_else(month(Date) >= 11,
-                          as.Date(paste("2021", month(Date), mday(Date), sep = "-")),
-                          as.Date(paste("2022", month(Date), mday(Date), sep = "-")))
+                          as.Date(paste("2022", month(Date), mday(Date), sep = "-")),
+                          as.Date(paste("2023", month(Date), mday(Date), sep = "-")))
   )
 
 # - Bed occupancy counts -
@@ -82,22 +84,22 @@ beds_england <- england %>%
   select(year, day_of_year, `Beds occupied` = `G&A beds occ'd`, `Beds free`, `CC beds occupied` = `CC Adult Occ`, `CC beds free`) %>%
   pivot_longer(cols = -c(year, day_of_year))
 
-beds_england_2122 <- beds_england %>%
+beds_england_current <- beds_england %>%
+  filter(year == "2022-23")
+
+beds_england_previous <- beds_england %>%
   filter(year == "2021-22")
 
-beds_england_2021 <- beds_england %>%
-  filter(year == "2020-21")
-
 beds_england_historical <- beds_england %>%
-  filter(!year %in% c("2021-22", "2020-21")) %>%
+  filter(!year %in% c("2022-23", "2021-22")) %>%
   group_by(name, day_of_year) %>%
   summarise(value = median(value)) %>%
   ungroup() %>%
-  mutate(year = "2015-16 to 2019-20")
+  mutate(year = "2015-16 to 2020-21")
 
 beds_england <- bind_rows(
-  beds_england_2122,
-  beds_england_2021,
+  beds_england_current,
+  beds_england_previous,
   beds_england_historical
 )
 
@@ -108,7 +110,7 @@ england_summary <-
 
   filter(
     # week == this_week,  # Get the latest week in the 2021-22 data and its equivalents in 2020-21 and 2019-20
-    year %in% c("2021-22", "2020-21", "2019-20")
+    year %in% c("2022-23", "2021-22", "2020-21", "2019-20")
   ) %>%
 
   # Calculate 7-day averages
@@ -125,13 +127,14 @@ england_summary <-
 # - Helper functions -
 get_trusts <- function(d) {
   d %>%
-    filter(!str_detect(Name, "ENGLAND")) %>%
+    filter(!str_detect(toupper(Name), "ENGLAND")) %>%
     select(Date, Code, Name, `Occupancy rate`, starts_with("G&A"), contains("CC"), contains("Critical"), contains("long"), Closures, Diverts, contains("Delays")) %>%
     mutate(across(-c(Date, Code, Name), as.double))
 }
 
 # - Get Trust bed occupancy data -
 trusts <- bind_rows(
+  get_trusts(sitrep2223),
   get_trusts(sitrep2122),
   get_trusts(sitrep2021),
   get_trusts(sitrep1920),
@@ -147,6 +150,7 @@ trusts <-
   trusts %>%
   mutate(
     year = case_when(
+      Date >= dmy("01-11-2022") & Date <= dmy("01-05-2023") ~ "2022-23",
       Date >= dmy("01-11-2021") & Date <= dmy("01-05-2022") ~ "2021-22",
       Date >= dmy("01-11-2020") & Date <= dmy("01-05-2021") ~ "2020-21",
       Date >= dmy("01-11-2019") & Date <= dmy("01-05-2020") ~ "2019-20",
@@ -156,8 +160,8 @@ trusts <-
       Date >= dmy("01-11-2015") & Date <= dmy("01-05-2016") ~ "2015-16"
     ),
     day_of_year = if_else(month(Date) >= 11,
-                          as.Date(paste("2021", month(Date), mday(Date), sep = "-")),
-                          as.Date(paste("2022", month(Date), mday(Date), sep = "-")))
+                          as.Date(paste("2022", month(Date), mday(Date), sep = "-")),
+                          as.Date(paste("2023", month(Date), mday(Date), sep = "-")))
   )
 
 # - Bed occupancy counts -
@@ -169,22 +173,22 @@ beds_trusts <- trusts %>%
   select(year, day_of_year, Code, Name, `Beds occupied` = `G&A beds occ'd`, `Beds free`, `CC beds occupied` = `CC Adult Occ`, `CC beds free`) %>%
   pivot_longer(cols = -c(year, day_of_year, Code, Name))
 
-beds_trusts_2122 <- beds_trusts %>%
+beds_trusts_current <- beds_trusts %>%
+  filter(year == "2022-23")
+
+beds_trusts_previous <- beds_trusts %>%
   filter(year == "2021-22")
 
-beds_trusts_2021 <- beds_trusts %>%
-  filter(year == "2020-21")
-
 beds_trusts_historical <- beds_trusts %>%
-  filter(!year %in% c("2021-22", "2020-21")) %>%
+  filter(!year %in% c("2022-23", "2021-22")) %>%
   group_by(Code, Name, name, day_of_year) %>%
   summarise(value = median(value)) %>%
   ungroup() %>%
-  mutate(year = "2015-16 to 2019-20")
+  mutate(year = "2015-16 to 2020-21")
 
 beds_trusts <- bind_rows(
-  beds_trusts_2122,
-  beds_trusts_2021,
+  beds_trusts_current,
+  beds_trusts_previous,
   beds_trusts_historical
 )
 
@@ -194,7 +198,7 @@ trusts_summary <-
   mutate(week = isoweek(Date)) %>%
 
   filter(
-    year %in% c("2021-22", "2020-21", "2019-20")
+    year %in% c("2022-23", "2021-22", "2020-21", "2019-20")
   ) %>%
 
   # Calculate 7-day averages
@@ -211,7 +215,7 @@ trusts_summary <-
 trusts_geocoded <-
   geographr::points_nhs_trusts22 |>
   right_join(
-    trusts |> filter(Date >= ymd("2021-11-01") & Date <= ymd("2022-05-01")),
+    trusts |> filter(Date >= ymd("2022-11-01") & Date <= ymd("2023-05-01")),
     by = c("nhs_trust22_code" = "Code")
   )
 
@@ -231,4 +235,4 @@ write_feather(beds_trusts, "data/trusts-beds.feather", compression = "uncompress
 write_feather(trusts_summary, "data/trusts-summary.feather", compression = "uncompressed")
 
 write_rds(trusts_geocoded, "data/trusts-geocoded.rds")
-write_rds(trusts_beds_geocoded, "data/trusts-beds-geocoded.rds")
+# write_rds(trusts_beds_geocoded, "data/trusts-beds-geocoded.rds")
